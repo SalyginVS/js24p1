0e751aac0436569349ce7a1f9a4065b87785ff3d
Vladimir <salyginvs@gmail.com>
2025-07-02 18:11:35 +0300

    added sprint 11 done

--- sprint_11/sprint_11.js
+++ sprint_11/sprint_11.js
@@ -2,7 +2,7 @@
 // Напишите функцию t01, которая добавляет .out-1 класс CSS (далее просто класс) 'bg-orange'.
 
 function t01 () {
-
+    document.querySelector('.out-1').classList.add('bg-orange');
 }
 
 document.querySelector('.b-1').onclick = t01;
@@ -11,7 +11,7 @@ document.querySelector('.b-1').onclick = t01;
 // Напишите функцию t02, которая удаляет у .out-2 класс CSS (далее просто класс) 'bg-orange'.
 
 function t02 () {
-
+    document.querySelector('.out-2').classList.remove('bg-orange');
 }
 
 document.querySelector('.b-2').onclick = t02;
@@ -20,7 +20,7 @@ document.querySelector('.b-2').onclick = t02;
 // Напишите функцию t03, которая проверяет наличие класса 'bg-orange' у .block-3. Результат проверки - выводит в .out-3.
 
 function t03 () {
-
+    document.querySelector('.out-3').textContent = document.querySelector('.block-3').classList.contains('bg-orange');
 }
 
 document.querySelector('.b-3').onclick = t03;
@@ -30,7 +30,7 @@ document.querySelector('.b-3').onclick = t03;
 // Напишите функцию t04, которая переключает (toggle) класс 'bg-orange' у элемента .out-4.
 
 function t04 () {
-
+    document.querySelector('.out-4').classList.toggle('bg-orange');
 }
 
 document.querySelector('.b-4').onclick = t04;
@@ -40,7 +40,8 @@ document.querySelector('.b-4').onclick = t04;
 // Напишите функцию t05, которая заменяет класс 'bg-orange' на класс 'bg-bisque'.
 
 function t05 () {
-
+    document.querySelector('.out-5').classList.replace('bg-orange', 'bg-bisque');
+    // console.log(document.querySelector('.out-5').classList)
 }
 
 document.querySelector('.b-5').onclick = t05;
@@ -50,7 +51,7 @@ document.querySelector('.b-5').onclick = t05;
 // Напишите функцию t06, которая делает toggle класса 'round-img' для изображения .img-6.
 
 function t06 () {
-
+    document.querySelector('.img-6').classList.toggle('round-img');
 }
 
 document.querySelector('.b-6').onclick = t06;
@@ -60,7 +61,7 @@ document.querySelector('.b-6').onclick = t06;
 // Напишите функцию t07, которая добавляет класс 'bold' в .out-7.
 
 function t07 () {
-
+    document.querySelector('.out-7').classList.add('bold');
 }
 
 document.querySelector('.b-7').onclick = t07;
@@ -70,7 +71,7 @@ document.querySelector('.b-7').onclick = t07;
 // Напишите функцию t08, которая переключает (toggle) класс 'hide' для блока .out-8.
 
 function t08 () {
-
+    document.querySelector('.out-8').classList.toggle('hide');
 }
 
 document.querySelector('.b-8').onclick = t08;
@@ -80,7 +81,15 @@ document.querySelector('.b-8').onclick = t08;
 // Напишите функцию t09, которая при нажатии кнопки проверяет input.ch-91 и если он выбран, то добавляет класс 'bold' для .out-9. Если input.ch-92 выбран - то добавляет класс 'uppercase' для .out-9. Если checkbox не выбраны, то удаляет данные классы у .out-9.
 
 function t09 () {
+    const ch91 = document.querySelector('input.ch-91');
+    const ch92 = document.querySelector('input.ch-92');
+    const out9 = document.querySelector('.out-9');
 
+    if (ch91.checked) out9.classList.add('bold');
+    else if (!ch91.checked) out9.classList.remove('bold');
+
+    if (ch92.checked) out9.classList.add('uppercase');
+    else if (!ch92.checked) out9.classList.remove('uppercase');
 }
 
 document.querySelector('.b-9').onclick = t09;
@@ -90,7 +99,7 @@ document.querySelector('.b-9').onclick = t09;
 // Напишите функцию t10, которая при нажатии кнопки делает toggle класса 'clip' для .out-10.
 
 function t10 () {
-
+    document.querySelector('.out-10').classList.toggle('clip');
 }
 
 document.querySelector('.b-10').onclick = t10;
