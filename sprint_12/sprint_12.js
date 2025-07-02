91bb50e873b1dae7dae7a3715b7121089d63978f
Vladimir <salyginvs@gmail.com>
2025-07-02 21:13:29 +0300

    added sprint 12 tasks from 1 to 9 done

--- sprint_12/sprint_12.js
+++ sprint_12/sprint_12.js
@@ -1,7 +1,8 @@
 // Task 1
 // В начальных спринтах мы создавали элементы с помощью innerHTML. Давайте вспомним как это было. Создайте заголовок второго уровня с текстом 'Sprint 12'. Поместите элемент в .out-1 с помощью innerHTML.
 
-function t01 () {
+function t01() {
+    document.querySelector('.out-1').innerHTML = '<h1>Sprint 12</h1>';
 }
 
 document.querySelector('.b-1').onclick = t01;
@@ -9,11 +10,11 @@ document.querySelector('.b-1').onclick = t01;
 // Task 2
 // Создание через innerHTML не гибкое, давайте научимся более гибким способам создания элементов в JavaScript. Напишите функцию t02, которая создает через createElement элемент h2, затем через textContent добавьте в созданный элемент текст 'Sprint 12', после чего добавьте через append элемент в .out-2.
 
-function t02 () {
- 
-
+function t02() {
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Sprint 12'
+    document.querySelector('.out-2').append(h2);
 }
-
 
 document.querySelector('.b-2').onclick = t02;
 
@@ -21,8 +22,10 @@ document.querySelector('.b-2').onclick = t02;
 // Task 3
 // Функция должна создавать через createElement элемент img, в качестве атрибута src укажите './images/harconen.png'. Добавьте через append созданное изображение в .out-3.
 
-function t03 () {
-  
+function t03() {
+    const img = document.createElement('img');
+    img.src = './images/harconen.png';
+    document.querySelector('.out-3').append(img);
 }
 
 
@@ -32,8 +35,11 @@ document.querySelector('.b-3').onclick = t03;
 // Task 4
 // Функция должна создавать через createElement элемент img, в качестве атрибута src укажите './images/harconen2.png'. Добавьте через classList класс 'round-img'.  Добавьте через append созданное изображение в .out-4.
 
-function t04 () {
-
+function t04() {
+    const img = document.createElement('img');
+    img.src = './images/harconen2.png';
+    img.classList.add('round-img');
+    document.querySelector('.out-4').append(img);
 }
 
 
@@ -43,8 +49,13 @@ document.querySelector('.b-4').onclick = t04;
 // Task 5
 // Функция должна создавать через createElement элемент input. Задайте атрибут type="number", задайте значение value="0", и добавьте через classList класс .i-5. Добавьте для элемента cвойство oninput равное t06. Через append добавьте элемент в .out-5. 
 
-function t05 () {
-
+function t05() {
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.value = '0';
+    input.classList.add('i-5');
+    input.oninput = t06;
+    document.querySelector('.out-5').append(input);
 }
 
 
@@ -54,7 +65,7 @@ document.querySelector('.b-5').onclick = t05;
 // Task 6
 // Данная фукнция автоматически будет срабатывать если таск 5 выполнен корректно.
 
-function t06 () {
+function t06() {
     document.querySelector('.out-6').textContent = document.querySelector('.i-5').value;
 }
 
@@ -62,8 +73,21 @@ function t06 () {
 // Task 7
 // Функция должна создавать через createElement элемент select  c классом 's-7'. Добавьте в созданный select элементы option с value = 22 и тектом 'Лето II Атрейдес' и option с value 33 и текстом 'Дункан Айдахо'. Создавайте option через createElement. Добавьте созданный select через append в .out-7.
 
-function t07 () {
-   
+function t07() {
+    const select = document.createElement('select');
+    const option1 = document.createElement('option');
+    const option2 = document.createElement('option');
+
+    select.classList.add('s-7');
+    option1.value = '22';
+    option1.text = 'Лето II Атрейдес';
+    option2.value = '33';
+    option2.text = 'Дункан Айдахо';
+
+    select.append(option1);
+    select.append(option2);
+
+    document.querySelector('.out-7').append(select);
 }
 
 
@@ -73,8 +97,12 @@ document.querySelector('.b-7').onclick = t07;
 // Task 8
 // Функция должна получать текст из input.i-8, создавать элемент li c помощью createElement и записывать в него полученный текст, после чего добавлять в .list-8 через append.
 
-function t08 () {
+function t08() {
+    let text = document.querySelector('input.i-8').value;
+    const li = document.createElement('li');
 
+    li.textContent = text;
+    document.querySelector('.list-8').append(li);
 }
 
 
@@ -84,7 +112,7 @@ document.querySelector('.b-8').onclick = t08;
 // Task 9
 // Функция должна создавать элемент 'details' через createElement, создайте через createElement элемент 'summary' с текстом 'close/open'. Добавьте через append созданный summary в details. Создайте текстовую ноду через createTextNode(), с текстом 'Dune is a 1965 epic science fiction novel by American author Frank Herbert, originally published as two separate serials in Analog magazine.' Добавьте созданную ноду в details через append. Созданный details в .out-9 через append.
 
-function t09 () {
+function t09() {
 
 }
 
@@ -94,8 +122,8 @@ document.querySelector('.b-9').onclick = t09;
 // Task 10
 // Функция должна создавать элемент 'tr' и два элемента 'td'. В первый добавьте текст 'Гхола', а во второй 'Гхола — клон, искусственное существо, которое выращивается в аксолотль-чане', добавьте оба td в tr через append. Созданный tr добавьте в таблицу .table-10 > tbody через append(). 
 
-function t10 () {
- 
+function t10() {
+
 }
 
 document.querySelector('.b-10').onclick = t10;
@@ -107,7 +135,7 @@ const div = document.createElement('div');
 div.textContent = 'Test div';
 div.classList.add('bg-orange');
 
-function t11 () {
+function t11() {
 }
 
 document.querySelector('.b-11').onclick = t11;
@@ -116,7 +144,7 @@ document.querySelector('.b-11').onclick = t11;
 // Task 12
 // Функция должна добавлять элемент div (созданный в таске 11) с помощью prepend в .out-11. Обратите внимание, куда добавляется элемент. 
 
-function t12 () {
+function t12() {
 }
 
 document.querySelector('.b-12').onclick = t12;
@@ -125,7 +153,7 @@ document.querySelector('.b-12').onclick = t12;
 // Task 13
 // Функция должна добавлять элемент div (созданный в таске 11) с помощью before в .out-11. Обратите внимание, куда добавляется элемент. 
 
-function t13 () {
+function t13() {
 }
 
 document.querySelector('.b-13').onclick = t13;
@@ -134,7 +162,7 @@ document.querySelector('.b-13').onclick = t13;
 // Task 14
 // Функция должна добавлять элемент div (созданный в таске 11) с помощью after в .out-11. Обратите внимание, куда добавляется элемент. 
 
-function t14 () {
+function t14() {
 }
 
 document.querySelector('.b-14').onclick = t14;
@@ -143,7 +171,7 @@ document.querySelector('.b-14').onclick = t14;
 // Task 15
 // Функция должна делать replaceWith для .out-15 заменяя его на div (созданный в таск 11). Обратите внимание, куда добавляется элемент. 
 
-function t15 () {
+function t15() {
 }
 
 document.querySelector('.b-15').onclick = t15;
@@ -152,7 +180,7 @@ document.querySelector('.b-15').onclick = t15;
 // Task 16
 // Функция должна делать remove() для элемента .out-16.
 
-function t16 () {
+function t16() {
 
 }
 
@@ -162,7 +190,7 @@ document.querySelector('.b-16').onclick = t16;
 // Тask 17
 // Как видно createElement очень гибок, а вместе с after, before, append, prepend позволяет очень гибко работать с DOM, однако не стоит списывать innerHTML со счетов. Давайте заменим блок .out-17 строкой '<mark class="m-17">Thufir Hawat</mark>' с помощью outerHTML.
 
-function t17 () {
+function t17() {
 
 }
 
@@ -171,7 +199,7 @@ document.querySelector('.b-17').onclick = t17;
 // Тask 18
 // Функция t18 должна заменять блок .out-18 строкой '<mark class="m-18">Thufir Hawat</mark>' с помощью outerText.
 
-function t18 () {
+function t18() {
 
 }
 
@@ -180,7 +208,7 @@ document.querySelector('.b-18').onclick = t18;
 // Тask 19
 // Изучите работу метода insertAdjacentHTML на примере функции t19. Запустите, изучите ее работу с опцией 'beforebegin'. Замените опцию 'beforebegin' на 'afterbegin', 'beforeend', 'afterend'.
 
-function t19 () {
+function t19() {
     document.querySelector('.out-19').insertAdjacentHTML('beforebegin', '<mark>TEST</mark>');
 }
 
@@ -189,7 +217,7 @@ document.querySelector('.b-19').onclick = t19;
 // Тask 20
 // Изучите работу метода insertAdjacentText на примере функции t20. Запустите, изучите ее работу с опцией 'beforebegin'. Замените опцию 'beforebegin' на 'afterbegin', 'beforeend', 'afterend'.
 
-function t20 () {
+function t20() {
     document.querySelector('.out-20').insertAdjacentText('beforebegin', '<mark>TEST</mark>');
 }
 
